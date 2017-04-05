8b2bed09cde73b0f72e7e6c5138aa3d15b11402e
Gabriel-p <gabrielperren@gmail.com>
2017-04-05 16:54:50 -0300

    begin writing script

---
+++ tasks/data_avrg.py
@@ -0,0 +1,17 @@
+
+import astropy
+import numpy as np
+
+
+def main():
+    """
+    Goes through each sub-folder of the input folder given, reads all the
+    fwhm_final.dat files stored there. Obtains an average of the FWHM,
+    sky mean, and sky STDDEV values stored in each file.
+    """
+
+    print("Finished")
+
+
+if __name__ == '__main__':
+    main()
