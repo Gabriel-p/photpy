198aa20d416a0bc07f80fd460622c195374a936c
Gabriel-p <gabrielperren@gmail.com>
2017-05-05 17:55:18 -0300

    almost done with ext coefficients

--- tasks/ext_coeff.py
+++ tasks/ext_coeff.py
@@ -43,9 +43,9 @@ def main():
     """
     """
     pars, fits_list, in_out_path = in_params()
-    f_key, exp_key, air_key = pars['filter_key'][0], pars['exposure_key'][0],\
-        pars['airmass_key'][0]
-    landolt_fl = read_standard_coo(in_out_path, pars['landolt_fld'][0])
+    f_key, exp_key, air_key = pars['filter_key'], pars['exposure_key'],\
+        pars['airmass_key']
+    landolt_fl = read_standard_coo(in_out_path, pars['landolt_fld'])
 
     filters = {'U': [], 'B': [], 'V': [], 'R': [], 'I': []}
     for imname in fits_list:
@@ -59,19 +59,42 @@ def main():
 
         print("Aperture photometry on: {}".format(fname))
         photu = instrumMags(
-            landolt_fl, hdu_data, exp_time, float(pars['aperture'][0]),
-            float(pars['annulus_in'][0]), float(pars['annulus_out'][0]))
+            landolt_fl, hdu_data, exp_time, float(pars['aperture']),
+            float(pars['annulus_in']), float(pars['annulus_out']))
 
         stars = []
-        for inst_mag in photu['cal_mags'].value:
-            stars.append([inst_mag, airmass])
+        for i, inst_mag in enumerate(photu['cal_mags'].value):
+            stars.append([airmass, inst_mag, landolt_fl['ID'][i]])
 
         # Group frames by filter.
         filters[filt].append(stars)
 
-    import pdb; pdb.set_trace()  # breakpoint 1aa181a1 //
+    for filt, fdata in filters.iteritems():
+        if fdata:
+            print("Filter {}".format(filt))
+            cc = [zip(*_) for _ in fdata]
+            airm_mag = [[[], [], []] for _ in fdata[0]]
+            for a in cc:
+                aa = zip(*a)
+                for i, st in enumerate(aa):
+                    airm_mag[i][0].append(st[0])
+                    airm_mag[i][1].append(st[1])
+                    airm_mag[i][2].append(st[2])
 
+            yl = []
+            for st in airm_mag:
+                plt.scatter(st[0], st[1])
+                m, c, r_value, p_value, std_err = linregress(st[0], st[1])
+                print("Star {}, m={:.3f}".format(st[2][0], m))
+                yl.append(max(st[1]))
+                yl.append(min(st[1]))
+            plt.ylim(max(yl) + .05, min(yl) - .05)
+            plt.show()
+
+    import pdb; pdb.set_trace()  # breakpoint 1aa181a1 //
 
 
 if __name__ == '__main__':
     main()
+
+# https://arxiv.org/PS_cache/arxiv/pdf/0906/0906.3014v1.pdf
