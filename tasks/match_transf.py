2a97f139b7c1d8b53e662b46c827518ee79b745b
Gabriel-p <gabrielperren@gmail.com>
2017-06-02 17:59:36 -0300

    lots of improvements to match_transf

--- tasks/match_transf.py
+++ tasks/match_transf.py
@@ -1,8 +1,10 @@
 
 import operator
 import numpy as np
+import random
 from scipy.spatial.distance import cdist
 
+import timeit
 import matplotlib.pyplot as plt
 
 
@@ -10,18 +12,20 @@ def genData():
     """
     Generate random (x,y) coordinates
     """
+    random.seed(9001)
+    np.random.seed(117)
     frames = {'U': {'60': [], '100': [], '250': []},
               'B': {'30': [], '70': [], '200': []},
               'V': {'30': [], '70': [], '100': []},
               'I': {'30': [], '50': [], '100': []}}
     # Initial positions
-    x = np.random.uniform(0., 4000., 5000)
-    y = np.random.uniform(0., 4000., 5000)
+    x = np.random.uniform(0., 4000., 50000)
+    y = np.random.uniform(0., 4000., 50000)
     # Factor that determines the number of stars per filter.
     fact = {'U': [5, .5], 'B': [7, .7], 'V': [8, .8], 'I': [10, 1.]}
     for filt, expDict in frames.iteritems():
         for exps in expDict.keys():
-            N = int(float(exps) * np.random.randint(4, fact[filt][0]) *
+            N = 10 * int(float(exps) * np.random.randint(4, fact[filt][0]) *
                     fact[filt][1])
             # Sigma and mu for normal distribution
             sigma, mu = 1., 1.
@@ -29,6 +33,9 @@ def genData():
             noise = sigma * np.random.randn(len(x)) + mu
             x_n, y_n = x + noise, y + noise
             x_n, y_n = x_n[:N], y_n[:N]
+            xy_n = list(zip(*[x_n, y_n]))
+            random.shuffle(xy_n)
+            x_n, y_n = np.asarray(zip(*xy_n))
             mag = np.random.uniform(10., 24., N)
             e_mag = np.random.uniform(.01, .2, N)
             # plt.scatter(x, y)
@@ -99,15 +106,31 @@ def closestStar(x_fr1, y_fr1, x_fr2, y_fr2):
     """
     For every star in fr1, find the closest star in fr2.
 
-    * i: index of ith star in fr1.
-    * dist[i]: stores the distances from this star to *all* stars in fr2
-    * min_dist_idx[i]: index to the star in fr2 closest to the ith star in fr1
-      It is also the index of the minimum distance in dist[i], i.e.: distance
-      to the closest star in fr2 to the ith star in fr1.
-    * fr2[min_dist[i]]: closest star in fr2 to this star in fr1.
-    * dist[i][min_dist_idx[i]]: distance between these two stars.
+    Parameters
+    ----------
+    x_fr1 : list
+       x coordinates for stars in the reference frame.
+    y_fr1 : list
+       y coordinates for stars in the reference frame.
+    x_fr2 : list
+       x coordinates for stars in the processed frame.
+    y_fr2 : list
+       y coordinates for stars in the processed frame.
 
+    Returns
+    -------
+    min_dist_idx : numpy array
+        Index to the processed star closest to the reference star, for each
+        reference star:
+        * fr2[min_dist_idx[i]]: closest star in fr2 to this star in fr1.
+        Also the index of the minimum distance in dist[i], i.e.: distance to
+        the closest processed star to the ith reference star:
+        * dist[i][min_dist_idx[i]]: distance between these two stars.
+
+    Notes
+    -----
     len(fr1) = len(dist) = len(min_dist_idx)
+
 
     """
     fr1 = np.array(zip(*[x_fr1, y_fr1]))
@@ -127,44 +150,164 @@ def starMatch(c1_ids, c2_ids, d2d, maxrad):
     Reject duplicated matches and matched stars with distances beyond
     the maximum separation defined.
 
-    * match_c1_ids, match_c2_ids: ids for unique matches between frames
-    * no_match_c1: ids of reference stars with no match found
-    * dupl_c1_ids: ids of reference stars with a duplicated match that will
-      be re-processed.
+    Parameters
+    ----------
+    c1_ids : list
+        IDs of stars in the reference frame.
+    c2_ids : list
+        IDs of stars in the processed frame, closest to each star in the
+        reference frame.
+    d2d : list
+        Distances between each star in the reference frame, and the closest
+        star in the processed frame.
+    maxrad : float
+        Maximum allowed distance (radius) for a match to be valid.
+
+    Returns
+    -------
+    match_c1_ids : list
+        IDs in reference frame for unique matches between frames.
+    match_c2_ids : list
+        IDs in the processed frame for unique matches between frames.
+    no_match_c1 : list
+        IDs of reference stars with no match found within maxrad.
+    dupl_c1_ids : list
+        IDs of reference stars that were matched to the same processed star as
+        another reference star, and had a larger distance. These stars will
+        be re-processed.
+
     """
 
-    match_c1_ids, dupl_c1_ids, no_match_c1 = [], [], []
-    match_c2_ids, match_d2d, no_match_d2d = [], [], []
+    match_c1_ids, dupl_c1_ids, match_c2_ids, match_d = [], [], [], []
     # Indexes of matched star in both frames and distance between them.
     for c1_i, c2_i, d in zip(*[c1_ids, c2_ids, d2d]):
         # Filter by maximum allowed match distance.
         if d <= maxrad:
-            # Check if this queried star in c2 was already stored.
+            # Check if this processed star was already stored as a match with
+            # another reference star.
             if c2_i in match_c2_ids:
                 # Index of this stored c2 star.
                 j = match_c2_ids.index(c2_i)
-                # Only replace with this match if the previous match had a
-                # larger distance.
-                if match_d2d[j] > d:
-                    # Store rejected replaced star.
+                # If the previous match had a larger distance than this match,
+                # replace with this match.
+                if match_d[j] > d:
+                    # Store replaced reference star here.
                     dupl_c1_ids.append(match_c1_ids[j])
-                    # Replace star
+                    # Now replace this star.
                     match_c1_ids[j] = c1_i
-                    match_d2d[j] = d
+                    match_d[j] = d
                 else:
                     dupl_c1_ids.append(c1_i)
             else:
+                # Store IDs of both matched stars, and their distance.
                 match_c1_ids.append(c1_i)
                 match_c2_ids.append(c2_i)
-                match_d2d.append(d)
-        else:
-            # If this observed star has no queried star closer than the max
-            # distance allowed, discard.
-            no_match_c1.append(c1_i)
-            no_match_d2d.append(d)
+                match_d.append(d)
+        # else:
+            # This reference star has no processed star closer than the max
+            # distance allowed.
 
-    return match_c1_ids, match_c2_ids, no_match_c1, dupl_c1_ids, match_d2d,\
-        no_match_d2d
+    return dupl_c1_ids, match_c1_ids, match_c2_ids, match_d
+
+
+def frameCoordsUpdt(x_fr, y_fr, match_fr2_ids):
+    """
+    Identify stars in the processed frame that where not matched to any star
+    in the reference frame.
+    To avoid messing with the indexes, change the coordinates of already
+    matched 'frame' stars so that they will not be matched again.
+
+    Parameters
+    ----------
+    x_fr : list
+        Original x coordinates of the stars in the processed frame.
+    y_fr : list
+        Original y coordinates of the stars in the processed frame.
+    match_fr2_ids : list
+        IDs of stars in the processed frame that were matched to a star in
+        the reference frame.
+
+    Returns
+    -------
+    x_fr_updt, y_fr_updt : list, list
+        Coordinates of frame stars with those identified as matches changed
+        so that they will not be matched again.
+
+    """
+    # Modify coordinates of matched stars. Use copy of arrays to avoid
+    # overwriting the original coordinate values in 'frame'.
+    x_fr_updt, y_fr_updt = np.copy(x_fr), np.copy(y_fr)
+    x_fr_updt[match_fr2_ids] = -1000.
+    y_fr_updt[match_fr2_ids] = -1000.
+
+    return x_fr_updt, y_fr_updt
+
+
+def UpdtRefFrame(refFrameInfo, refFrame, frame, match_fr1_ids_all,
+                 match_fr2_ids_all):
+    """
+    Update the reference frame adding the stars in the processed frame that
+    were assigned as matches to each reference star. If a reference star was
+    not assigned any match from the processed frame, add a Nan value.
+
+    Also add to the end of the list (thereby increasing the length of the
+    reference frame) those processed stars that could not be matched to any
+    reference star.
+
+    Parameters
+    ----------
+    """
+    # Extract processed frame data.
+    fr_filt, fr_expTime = frame[:2]
+    x_fr, y_fr, mag_fr, emag_fr = frame[2]
+
+    start_time = timeit.default_timer()
+    # for each reference frame star.
+    for ref_id, ref_st in enumerate(refFrame):
+        # Check if this reference star was uniquely associated with a
+        # processed star.
+        if ref_id in match_fr1_ids_all:
+            # Index of the associated processed star.
+            j = match_fr1_ids_all.index(ref_id)
+            fr_id = match_fr2_ids_all[j]
+            #
+            ref_st[0].append(x_fr[fr_id])
+            ref_st[1].append(y_fr[fr_id])
+            ref_st[2].append(mag_fr[fr_id])
+            ref_st[3].append(emag_fr[fr_id])
+        else:
+            # If this reference star could not be matched to any processed star
+            # within the maximum match radius defined, add a NaN value
+            # to mean that no match in the processed frame was found
+            # for this reference star.
+            ref_st[0].append(np.nan)
+            ref_st[1].append(np.nan)
+            ref_st[2].append(np.nan)
+            ref_st[3].append(np.nan)
+    print("C1", timeit.default_timer() - start_time)
+
+    # Number of frames processed this far including the reference frame, but
+    # excluding this one.
+    N_fr = len(refFrame[0][0]) - 1
+
+    start_time = timeit.default_timer()
+    # For each processed frame star.
+    fr_st_no_match = 0
+    for fr_id, fr_st in enumerate(zip(*[x_fr, y_fr, mag_fr, emag_fr])):
+        if fr_id not in match_fr2_ids_all:
+            # This frame star was not matched to any reference star.
+            x = [np.nan for _ in range(N_fr)] + [fr_st[0]]
+            y = [np.nan for _ in range(N_fr)] + [fr_st[1]]
+            mag = [np.nan for _ in range(N_fr)] + [fr_st[2]]
+            emag = [np.nan for _ in range(N_fr)] + [fr_st[3]]
+            refFrame.append([x, y, mag, emag])
+            fr_st_no_match += 1
+    print("C2", timeit.default_timer() - start_time)
+
+    # Update the information stored on the frames processed.
+    refFrameInfo.append([fr_filt, fr_expTime, fr_st_no_match])
+
+    return refFrameInfo, refFrame
 
 
 def main():
@@ -177,7 +320,9 @@ def main():
     refFrameInfo, refFrame, framesOrdered = framesOrder(frames)
 
     for frame in framesOrdered:
-        print("\nReference frame:")
+        print("\n--------------------------------------")
+        print("Reference frame (N={}), composed of:".format(
+            np.sum(zip(*refFrameInfo)[2])))
         for _ in refFrameInfo:
             print("{}, {} (N={})".format(*_))
 
@@ -188,65 +333,70 @@ def main():
             x_ref.append(np.mean(st[0]))
             y_ref.append(np.mean(st[1]))
 
+        # Extract filter name and exposure time of the processed frame.
         fr_filt, fr_expTime = frame[:2]
-        x_fr_orig, y_fr_orig = frame[2][:2]
-        # Copy of original lists, as these will be edited.
-        x_fr, y_fr = list(x_fr_orig), list(y_fr_orig)
-        print('\nProcessing frame: {}, {} (N={})\n'.format(
+        x_fr, y_fr = frame[2][:2]
+        print('\nProcessing frame: {}, {} (N={})'.format(
             fr_filt, fr_expTime, len(x_fr)))
 
         # Initial full list of IDs for the reference and processed frame.
-        fr1_ids = [_ for _ in range(len(refFrame))]
-        fr2_ids = [_ for _ in range(len(x_fr_orig))]
+        fr1_ids = np.arange(len(refFrame)).tolist()
+        fr2_ids = np.arange(len(x_fr)).tolist()
 
-        match_fr1_ids_all, match_fr2_ids_all, no_match_fr1_all, match_d2d_all,\
-            no_match_d2d_all = [], [], [], [], []
+        match_fr1_ids_all, match_fr2_ids_all, match_d_all = [], [], []
         # Continue until no more duplicate matches exist.
-        i_while = 1
+        counter = 1
         while fr1_ids:
 
-            # Match reference and processed frame.
+            start_time = timeit.default_timer()
+            # Find closest stars between reference and processed frame.
             fr2_ids_dup, fr1fr2_d2d = closestStar(x_ref, y_ref, x_fr, y_fr)
+            print("A", timeit.default_timer() - start_time)
 
-            match_fr1_ids, match_fr2_ids, no_match_fr1, fr1_ids, match_d2d,\
-                no_match_d2d = starMatch(
-                    fr1_ids, fr2_ids_dup, fr1fr2_d2d, maxrad)
-            # Store all unique solutions and no match solutions.
+            start_time = timeit.default_timer()
+            # Match reference and processed frame.
+            fr1_ids, match_fr1_ids, match_fr2_ids, match_d =\
+                starMatch(fr1_ids, fr2_ids_dup, fr1fr2_d2d, maxrad)
+            # Store unique matches and distances.
             match_fr1_ids_all += match_fr1_ids
             match_fr2_ids_all += match_fr2_ids
-            no_match_fr1_all += no_match_fr1
-            match_d2d_all += match_d2d
-            no_match_d2d_all += no_match_d2d
+            match_d_all += match_d
+            print("B", timeit.default_timer() - start_time)
 
-            print("{}.".format(i_while))
+            print("{}.".format(counter))
+            counter += 1
+
             print("Matched reference stars: {}".format(len(match_fr1_ids)))
             if match_fr1_ids:
                 print("(Mean match dist: {:.2f} px)".format(
-                    np.mean(match_d2d_all)))
+                    np.mean(match_d_all)))
             print("Reference stars w/ no match within maxrad: {}".format(
-                len(no_match_fr1)))
+                len(refFrame) - len(match_fr1_ids_all) - len(fr1_ids)))
 
+            # If there are any stars from the reference frame that had
+            # duplicated matches and were stored for re-matching.
             if fr1_ids:
                 print("Reference stars for re-match: {}".format(
                     len(fr1_ids)))
-                # 'frame' stars that were not matched to an observed star.
-                fr2_ids_r = [_ for _ in fr2_ids if _ not in match_fr2_ids_all]
-                print("Frame stars for re-match: {}".format(len(fr2_ids_r)))
-                # To avoid messing with the indexes, change the coordinates
-                # of already matched 'frame' stars so that they can not
-                # possibly be matched again.
-                x_fr, y_fr = [], []
-                for c2_i in fr2_ids:
-                    if c2_i in match_fr2_ids_all:
-                        x_fr.append(-1000.)
-                        y_fr.append(-1000.)
-                    else:
-                        x_fr.append(x_fr_orig[c2_i])
-                        y_fr.append(y_fr_orig[c2_i])
-            i_while += 1
+                print("Frame stars for re-match: {}".format(
+                    len(fr2_ids) - len(match_fr2_ids_all)))
+                # Update coordinates of matched stars in processed frame.
+                x_fr, y_fr = frameCoordsUpdt(x_fr, y_fr, match_fr2_ids)
+            else:
+                print("Processed stars w/ no match within maxrad: {}".format(
+                    len(fr2_ids) - len(match_fr2_ids_all)))
 
-        print('\nTotal stars matched: {}'.format(len(match_fr1_ids_all)))
-        print('Total stars not matched: {}'.format(len(no_match_fr1_all)))
+        # Update reference frame associating all the matches found in the
+        # processed frame to a given reference star. Also append those stars
+        # from the processed frame with no match to the end of the list.
+        refFrameInfo, refFrame = UpdtRefFrame(
+            refFrameInfo, refFrame, frame, match_fr1_ids_all,
+            match_fr2_ids_all)
+
+    print("\nFinal combined reference frame (N={})".format(
+        np.sum(zip(*refFrameInfo)[2])))
+    for _ in refFrameInfo:
+        print("{}, {} (N={})".format(*_))
 
 
 if __name__ == '__main__':
