0a9b509370fd8a36aeaa0d5fa2d91d7e8518e3e3
Gabriel-p <gabrielperren@gmail.com>
2018-01-22 12:06:34 -0300

    new script: use file created by match

--- tasks/ext_coeff.py
+++ tasks/ext_coeff.py
@@ -1,62 +1,49 @@
 
 import read_pars_file as rpf
-from aperphot_standards import instrumMags
-from hlpr import st_fwhm_select
+from aperphot_standards import read_standard_coo, instrumMags
 
 import os
 from os.path import join, isfile
 import numpy as np
 from scipy.stats import linregress
 import matplotlib.pyplot as plt
-
 from astropy.io import fits
-from astropy.table import Table
 
 
-def in_params():
+def in_params(in_fold, ldlt_fold):
     """
     Read and prepare input parameter values.
     """
     pars = rpf.main()
 
-    in_out_path = pars['mypath'].replace('tasks', 'output/standards')
+    in_path = pars['mypath'].replace('tasks', 'input/' + in_fold)
+    stnd_path = pars['mypath'].replace('tasks', ldlt_fold)
 
     fits_list = []
-    for file in os.listdir(in_out_path):
-        f = join(in_out_path, file)
+    for file in os.listdir(in_path):
+        f = join(in_path, file)
         if isfile(f):
             if f.endswith('.fits'):
                 fits_list.append(f)
 
-    return pars, fits_list, in_out_path
+    return pars, in_path, stnd_path, fits_list
 
 
-def main():
+def main(in_fold, ldlt_fold, stnd_fl):
     """
     Rough sketch of script to determine extinction coefficients.
     """
-    pars, fits_list, in_out_path = in_params()
+    pars, in_path, stnd_path, fits_list = in_params(in_fold, ldlt_fold)
     f_key, exp_key, air_key = pars['filter_key'], pars['exposure_key'],\
         pars['airmass_key']
 
-    ffile = 'stk_2083_crop.fits'
-    print("Obtain standard stars coordinates from: {}".format(ffile))
-    # Perform a Daofind on a selected .fits frame.
-    hdulist = fits.open(join(in_out_path, ffile))
-    hdu_data = hdulist[0].data
-    psf_select = st_fwhm_select(
-        float(pars['dmax']), int(pars['max_stars']),
-        float(pars['thresh_fit']), float(pars['fwhm_init']), 35.,
-        hdu_data)[0]
-    psf_select['ID'], psf_select['x_obs'], psf_select['y_obs'] =\
-        psf_select['id'], psf_select['xcentroid'], psf_select['ycentroid']
-    # This table holds the IDs and coordinates of all stars detected.
-    landolt_fl = Table([
-        psf_select['ID'], psf_select['x_obs'], psf_select['y_obs']])
+    print("Obtain standard stars coordinates from: {}".format(stnd_fl))
+    # Read data for this Landolt field.
+    landolt_fl = read_standard_coo(stnd_path, stnd_fl)
 
     filters = {'U': [], 'B': [], 'V': [], 'R': [], 'I': []}
     for imname in fits_list:
-        fname = imname.replace(pars['mypath'].replace('tasks', 'output'), '')
+        f_name = imname.split('/')[-1].split('.')[0]
 
         # Load .fits file.
         hdulist = fits.open(imname)
@@ -64,13 +51,14 @@ def main():
         hdr, hdu_data = hdulist[0].header, hdulist[0].data
         filt, exp_time, airmass = hdr[f_key], hdr[exp_key], hdr[air_key]
 
-        print("Aperture photometry on: {}".format(fname))
+        print("Aperture photometry on: {}".format(f_name))
         photu = instrumMags(
-            landolt_fl, hdu_data, exp_time, float(pars['aperture']),
+            f_name, landolt_fl, hdu_data, exp_time, float(pars['aperture']),
             float(pars['annulus_in']), float(pars['annulus_out']))
 
         stars = []
         for i, inst_mag in enumerate(photu['cal_mags'].value):
+            print(filt, landolt_fl['ID'][i], airmass, inst_mag)
             stars.append([airmass, inst_mag, landolt_fl['ID'][i]])
 
         # Group frames by filter.
@@ -79,6 +67,21 @@ def main():
     for filt, fdata in filters.iteritems():
         if fdata:
             print("Filter {}".format(filt))
+
+            # airm, mag = [], []
+            # for f in fdata:
+            #     airm += [_[0] for _ in f]
+            #     mag += [_[1] for _ in f]
+
+            # m = linregress(airm, mag)[0]
+            # print(" K: {:.3f}".format(m))
+
+            # if not np.isnan(m):
+            #     plt.style.use('seaborn-darkgrid')
+            #     plt.title("Filter {}".format(filt))
+            #     plt.scatter(airm, mag)
+            #     plt.show()
+
             # Transform list into into:
             # airm_mag = [st1, st2, st3, ...]
             # stX = [airmass, instrum_magnitudes]
@@ -87,26 +90,39 @@ def main():
             plt.style.use('seaborn-darkgrid')
             slopes = []
             for st in airm_mag:
-                # plt.scatter(st[0], st[1])
                 m, c, r_value, p_value, std_err = linregress(st[0], st[1])
                 slopes.append(m)
                 print("  Star {}, K={:.3f}".format(st[2][0], m))
+                plt.scatter(st[0], st[1])
+                plt.show()
 
             # Obtain median of extinction coefficients for this filter.
             median_K = np.nanmedian(slopes)
             print("Filter {}, median K: {:.3f}".format(filt, median_K))
             # Plot coefficients.
-            plt.title("Filter {}".format(filt))
-            plt.ylim(0., 2 * median_K)
-            slopes = np.array(slopes)
-            slopes = slopes[~np.isnan(slopes)]
-            plt.scatter(range(len(slopes)), slopes)
-            plt.axhline(y=median_K, color='r')
-            plt.show()
+            if not np.isnan(median_K):
+                plt.title("Filter {}".format(filt))
+                plt.ylim(0., 2 * median_K)
+                slopes = np.array(slopes)
+                slopes = slopes[~np.isnan(slopes)]
+                plt.scatter(range(len(slopes)), slopes)
+                plt.axhline(y=median_K, color='r')
+                plt.show()
 
 
 if __name__ == '__main__':
-    main()
+    """
+    To run this script we need *at least* two observations for each filter
+    with different airmass values.
+    """
+
+    # Name of the Landolt frame.
+    stnd_fl = 'SA95'
+    # Path of folder where .fits files are stored.
+    in_path = 'standards_95'
+    # Path to the _match.coo file for this frame.
+    ldlt_fold = 'output'
+    main(in_path, ldlt_fold, stnd_fl)
 
 # https://arxiv.org/PS_cache/arxiv/pdf/0906/0906.3014v1.pdf
 # v3 = +0.16, b3 = +0.25, i3 = +0.08, u3 = +0.45
