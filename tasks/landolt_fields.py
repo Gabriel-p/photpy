219e7c8d43de0e4ece6a938adaa27998acfbe35c
Gabriel-p <gabrielperren@gmail.com>
2018-01-22 08:47:05 -0300

    call zoom function

--- tasks/landolt_fields.py
+++ tasks/landolt_fields.py
@@ -1,5 +1,6 @@
 
 from astropy.table import Table
+from plot_events import zoom
 
 
 def getTable(data_rows):
@@ -175,39 +176,7 @@ if __name__ == '__main__':
         else:
             pass
 
-    def zoom_fun(event):
-        """
-        https://gist.github.com/tacaswell/3144287
-        """
-        base_scale = 2.
-        # get the current x and y limits
-        cur_xlim = ax.get_xlim()
-        cur_ylim = ax.get_ylim()
-        # set the range
-        cur_xrange = (cur_xlim[1] - cur_xlim[0])*.5
-        cur_yrange = (cur_ylim[1] - cur_ylim[0])*.5
-        xdata = event.xdata # get event x location
-        ydata = event.ydata # get event y location
-        if event.button == 'up':
-            # deal with zoom in
-            scale_factor = 1/base_scale
-        elif event.button == 'down':
-            # deal with zoom out
-            scale_factor = base_scale
-        else:
-            # deal with something that should never happen
-            scale_factor = 1
-            print event.button
-        # set new limits
-        ax.set_xlim([xdata - cur_xrange*scale_factor,
-                     xdata + cur_xrange*scale_factor])
-        ax.set_ylim([ydata - cur_yrange*scale_factor,
-                     ydata + cur_yrange*scale_factor])
-        ax.figure.canvas.draw() # force re-draw
-
-    # attach the call back
-    fig.canvas.mpl_connect('scroll_event', zoom_fun)
-
+    fig.canvas.mpl_connect('scroll_event', lambda event: zoom(event, [ax]))
     fig.canvas.mpl_connect('button_press_event', onclick)
     plt.show()
 
